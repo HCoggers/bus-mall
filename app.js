0df74b905c3c16c2acd3340f68ac196072c3f348
HCoggers <harry.cogswell@gmail.com>
2019-01-21 17:05:48 -0800

    added picture functionality in JS

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var allProducts = [];
-var productA = document.getElementById('producta');
-var productB = document.getElementById('productb');
-var productC = document.getElementById('productc');
 
+var options = document.getElementById('products');
+var imgA = document.getElementById('producta');
+var imgB = document.getElementById('productb');
+var imgC = document.getElementById('productc');
+
+var randomize = [0, 0, 0];
+var pastRandomize = [0, 0, 0];
+
+var userClicks = 0;
 //Product Constructor function
 function Product(name) {
     this.title = name;
@@ -15,38 +21,80 @@ function Product(name) {
 }
 
 //Create all product objects
-var productNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
-for (var i = 0; i < (productNames.length - 1); i++) {
-    new Product(productNames[i]);
+var productFiles = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+var productTitles = ['R2D2 Luggage', 'Banana Slicer', 'Bathroom Tablet Stand', 'Useless Rainboots', 'All-in-one Breakfast Maker', 'Meatball Bubblegum', 'Uncomfortable Chair', 'Cthulhu Action Figure', 'Doggy Duck-beak', 'Dragon Meat', 'Pen utensils', 'Puppy Sweeper Sweater', 'Pizza Scissors', '']
+
+for (var i = 0; i < productFiles.length; i++) {
+    new Product(productFiles[i], productTitles[i]);
 }
 
 //Render random 3 images
-function renderProducts() {
-    var randomA = Math.floor(Math.random() * allProducts.length);
-    var randomB = Math.floor(Math.random() * allProducts.length);
-    var randomC = Math.floor(Math.random() * allProducts.length);
-    while (randomB === randomA) {
-        randomB = Math.floor(Math.random() * allProducts.length);
+function verifyRandoms() {
+    while (randomize[0] === pastRandomize[0] || randomize[0] === pastRandomize[1] || randomize[0] === pastRandomize[2]) {
+        randomize[0] = Math.floor(Math.random() * allProducts.length);
     }
-    while (randomC === randomA || randomC === randomB) {
-        randomC = Math.floor(Math.random() * allProducts.length);
+    while (randomize[1] === randomize[0] || randomize[1] === pastRandomize[0] || randomize[1] === pastRandomize[1] || randomize[1] === pastRandomize[2]) {
+        randomize[1] = Math.floor(Math.random() * allProducts.length);
     }
-    console.log('index numbers:', randomA, randomB, randomC);
+    while (randomize[2] === randomize[0] || randomize[2] === randomize[1] || randomize[2] === pastRandomize[0] || randomize[2] === pastRandomize[1] || randomize[2] === pastRandomize[2]) {
+        randomize[2] = Math.floor(Math.random() * allProducts.length);
+    }
+    // console.log('index numbers:', randomize[0], randomize[1], randomize[2]);
 
-    productA.src = allProducts[randomA].filepath;
-    productA.alt = allProducts[randomA].name;
-    productA.title = allProducts[randomA].name;
-    allProducts[randomA].views++;
-
-    productB.src = allProducts[randomB].filepath;
-    productB.alt = allProducts[randomB].name;
-    productB.title = allProducts[randomB].name;
-    allProducts[randomB].views++;
-
-    productC.src = allProducts[randomC].filepath;
-    productC.alt = allProducts[randomC].name;
-    productC.title = allProducts[randomC].name;
-    allProducts[randomC].views++;
+    pastRandomize[0] = randomize[0];
+    pastRandomize[1] = randomize[1];
+    pastRandomize[2] = randomize[2];
 }
 
+function renderProducts() {
+    imgA.src = allProducts[randomize[0]].filepath;
+    imgA.alt = allProducts[randomize[0]].title;
+    imgA.title = allProducts[randomize[0]].title;
+    allProducts[randomize[0]].views++;
+
+    imgB.src = allProducts[randomize[1]].filepath;
+    imgB.alt = allProducts[randomize[1]].title;
+    imgB.title = allProducts[randomize[1]].title;
+    allProducts[randomize[1]].views++;
+
+    imgC.src = allProducts[randomize[2]].filepath;
+    imgC.alt = allProducts[randomize[2]].title;
+    imgC.title = allProducts[randomize[2]].title;
+    allProducts[randomize[2]].views++;
+}
+
+//on page load:
+verifyRandoms();
 renderProducts();
+
+//when user clicks
+options.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    var clickPic = false;
+    for (var i = 0; i < allProducts.length; i++) {
+        if (event.target.title === allProducts[i].title) {
+            allProducts[i].clicks++;
+            userClicks++;
+            clickPic = true;
+            console.log(event.target.title);
+        }
+    }
+    if (clickPic === false) {
+        return alert('Please click on one of the products.');
+    }
+    if (userClicks === 25) {
+        console.table(allProducts);
+        options.removeEventListener('click', handleClick);
+
+        for (var i = 0; i < allProducts.length; i++) {
+            var ulEl = document.getElementById('results');
+            var liEl = document.createElement('li');
+            liEl.textContent = `${allProducts[i].clicks} votes for the ${allProducts[i].title}`;
+            ulEl.appendChild(liEl);
+        }
+        userClicks = 0;
+    }
+    verifyRandoms();
+    renderProducts();
+}
